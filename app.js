a1437e5923e4e178c57efffaffabf146668da3b1
tonyblankenship96 <tony.blankenship96@gmail.com>
2021-07-08 18:44:26 -0400

    finished

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-document.querySelector('section').innerText = 'Its alive'
+import DonutMaker from "./DonutMaker.js";
+
+const Donut = new DonutMaker(0, 0, 100, 1, 10);
+
+const donutBtnCount = document.querySelector(".donutButton"),
+  autoClickerBtnCount = document.querySelector(".autoClicker"),
+  multiplierBtnCount = document.querySelector(".multiplier");
+
+const donutCount = document.createElement("p");
+donutCount.innerText = "Donuts: " + Donut.numDonuts;
+donutBtnCount.appendChild(donutCount);
+
+const autoClickerPriceCount = document.createElement("p");
+autoClickerPriceCount.innerText =
+  "Auto Clicker Price :" +
+  Donut.autoClickerPrice +
+  " " +
+  "Auto Clickers: " +
+  Donut.numAutoClickers;
+autoClickerBtnCount.appendChild(autoClickerPriceCount);
+
+const multiplierPriceCount = document.createElement("p");
+multiplierPriceCount.innerText =
+  "Multiplier price: " +
+  Donut.clickMultiplierPrice +
+  " " +
+  "Multiplier: " +
+  Donut.clickMultiplier;
+multiplierBtnCount.appendChild(multiplierPriceCount);
